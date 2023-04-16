b82e9798e804a72d193438d96cc75ad9e4f8c437
Halina Tulchii <halendeihalyna@gmail.com>
2023-04-16 12:02:17 +0300

    update

---
+++ js/print.js
@@ -0,0 +1,12 @@
+// const btn = document.querySelector('.cv__button');
+
+// btn.onclick = () => {
+//     window.print();
+// }
+
+
+// btn.addEventListener('click', onClick);
+
+// function onClick() {
+//     window.print();
+// }
